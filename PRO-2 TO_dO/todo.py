f378ae3d0488df0da6824b68e2c42cfa47341358
venkataguruprasadG <venkatagp27@gmail.com>
2025-05-26 11:52:47 +0530

    Remove Feature

--- PRO-2 TO_dO/todo.py
+++ PRO-2 TO_dO/todo.py
@@ -3,12 +3,36 @@ tasks=[]
 while True:
     print("\n Your tasks: ",tasks)
 
-    action = input("Type add to add new tasks || 'quit' to to exit")
+    if not tasks:
+        print("No tasks yet..!!")
+    else:
+        for i, task in enumerate(tasks, 1):
+            print(f" {i}. {task}")
+    
+    print("\nOptions: ")
+    print("Type 'add' to add a new task")
+    print("Type 'Remove' to remove a task")
+    print("Type 'quit' to quit from the app")
 
+    action = input("Type about the task what you want to do ?")
+    
     if action == "add":
         task = input("Enter your task: ")
         tasks.append(task)
         print("Task is added")
+    elif action == "remove":
+        if not tasks:
+            print("No tasks to remove")
+            continue
+        num = input("Enter the number of the task you want to remove: ")
+        if num.isdigit():
+            num = int(num)
+            if 1<= num <= len(tasks):
+                removed = tasks.pop(num-1)
+            else:
+                print("Invalid Task Number: ",num)
+        else:
+            print("Enter the valid task number")
     elif action == "quit":
         print("Goodbye!!!")
         break

3652321cf64c87e85dd600047df2c7544187cbf9
venkataguruprasadG <venkatagp27@gmail.com>
2025-05-26 14:56:27 +0530

    Given access to the file

--- PRO-2 TO_dO/todo.py
+++ PRO-2 TO_dO/todo.py
@@ -1,40 +1,62 @@
-tasks=[]
+TASKS_FILE = "tasks.txt"
 
-while True:
-    print("\n Your tasks: ",tasks)
+def load_tasks():
+    tasks = []
+    try:
+        with open(TASKS_FILE, "r") as file:
+            for line in file:
+                tasks.append(line.strip())
+    except FileNotFoundError:
+        pass
+    return tasks
 
-    if not tasks:
-        print("No tasks yet..!!")
-    else:
-        for i, task in enumerate(tasks, 1):
-            print(f" {i}. {task}")
-    
-    print("\nOptions: ")
-    print("Type 'add' to add a new task")
-    print("Type 'Remove' to remove a task")
-    print("Type 'quit' to quit from the app")
+def save_tasks(tasks):
+    with open(TASKS_FILE, "w") as file:
+        for task in tasks:
+            file.write(task + "\n")
 
-    action = input("Type about the task what you want to do ?")
-    
-    if action == "add":
-        task = input("Enter your task: ")
-        tasks.append(task)
-        print("Task is added")
-    elif action == "remove":
+def main():
+    tasks = load_tasks()  # tasks is defined here
+
+    while True:
+        print("\nYour tasks:")
         if not tasks:
-            print("No tasks to remove")
-            continue
-        num = input("Enter the number of the task you want to remove: ")
-        if num.isdigit():
-            num = int(num)
-            if 1<= num <= len(tasks):
-                removed = tasks.pop(num-1)
-            else:
-                print("Invalid Task Number: ",num)
+            print("  (No tasks yet)")
         else:
-            print("Enter the valid task number")
-    elif action == "quit":
-        print("Goodbye!!!")
-        break
-    else:
-        print("Unknown Command. Try Again")
+            for i, task in enumerate(tasks, 1):
+                print(f"  {i}. {task}")
+
+        print("\nOptions:")
+        print("  add    - Add a new task")
+        print("  remove - Remove a task by its number")
+        print("  quit   - Exit the app")
+        action = input("What do you want to do? ")
+
+        if action == "add":
+            task = input("Enter your task: ")
+            tasks.append(task)
+            save_tasks(tasks)
+            print("Task added!")
+        elif action == "remove":
+            if not tasks:
+                print("No tasks to remove.")
+                continue
+            num = input("Enter the number of the task to remove: ")
+            if num.isdigit():
+                num = int(num)
+                if 1 <= num <= len(tasks):
+                    removed = tasks.pop(num - 1)
+                    save_tasks(tasks)
+                    print(f"Removed: {removed}")
+                else:
+                    print("Invalid task number.")
+            else:
+                print("Please enter a valid number.")
+        elif action == "quit":
+            print("Goodbye!")
+            break
+        else:
+            print("Unknown command. Try again.")
+
+if __name__ == "__main__":
+    main()
